6eb5389a8ace3652bda29532646c6c57557e75c5
Sunny Ahuwanya <sahuwanya@gmail.com>
2014-01-06 22:37:56 -0500

    Added Sample Console Client

---
+++ src/RabbitMQ/Clients/ConsoleClient/ConsoleClientExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using RestBus.RabbitMQ;
+using RestBus.RabbitMQ.Client;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var amqpUrl = "amqp:localhost:5672"; //AMQP URL for RabbitMQ installation
+            var serviceName = "madagascar"; //The unique identifier for the target service
+
+            var msgMapper = new BasicMessageMapper(amqpUrl, serviceName);
+
+            RestBusClient client = new RestBusClient(msgMapper);
+
+            RequestOptions requestOptions = null;
+            /* 
+             * //Uncomment this section to get a response in JSON format
+             * 
+            requestOptions = new RequestOptions();
+            requestOptions.Headers.Add("Accept", "application/json");
+             */
+
+            //Send Request
+            var response = client.GetAsync("/hello/random", requestOptions).Result;
+
+            //Display reponse;
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+            client.Dispose();
+            Console.ReadKey();
+
+        }
+    }
+}
